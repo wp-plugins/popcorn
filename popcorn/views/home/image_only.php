63d30cb9019c67976f6c34db46b9072fa7e93155
niravdave <niravdave@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-30 08:19:25 +0000

    Uploading Files
    git-svn-id: https://plugins.svn.wordpress.org/popcorn/tags/1.0.2@1123439 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ popcorn/views/home/image_only.php
@@ -0,0 +1,14 @@
+
+<div class="mainbox fleft slideup_plg">
+
+	<div class="header buttontab" style="background-color: <?php echo get_option('IOPD_heading_background_color').' !important'; ?>; " >
+		<span class="heading-text" style="color: <?php echo get_option('IOPD_heading_text_color').' !important'; ?>;" ><?php _e(get_option('IOPD_heading'),'popcorn');  ?></span><a href="#"><img src="<?php echo POPCORN__PLUGIN_URL.'assets/images/close.png'; ?>" class="close-icon"></a>
+	</div>
+
+	<div class="midarea conts" id="btn-1" style="background-color : <?php echo get_option('IOPD_body_background_color').' !important'; ?>;">
+		<p>
+			<a href="<?php echo get_option('IOPD_image_href'); ?>"><img src="<?php echo get_option('IOPD_image_src'); ?>" /></a>
+		</p>
+	</div>
+
+</div>
